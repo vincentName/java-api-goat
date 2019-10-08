71718b51420dbe039593924f0790f0f5799563c2
Rob Layzell <rlayzell@veracode.com>
2019-10-08 10:46:59 -0700

    Added example CWE-80 flaw to HelloController.

--- src/main/java/com/veracode/iast/samples/apigoat/HelloController.java
+++ src/main/java/com/veracode/iast/samples/apigoat/HelloController.java
@@ -2,13 +2,20 @@ package com.veracode.iast.samples.apigoat;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
+
+    private static final String template = "Hello, %s!";
 
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
 
+    @RequestMapping("/hello")
+	String test(@RequestParam(value="name", defaultValue="Noname") String param) {
+		return String.format(template, param);
+	}
 }
--- src/test/java/com/veracode/iast/samples/apigoat/HelloControllerTest.java
+++ src/test/java/com/veracode/iast/samples/apigoat/HelloControllerTest.java
@@ -23,9 +23,23 @@ public class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void getHello() throws Exception {
+    public void getRoot() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+    }
+
+    @Test
+    public void getHelloDefault() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello, Noname!")));
+    }
+
+    @Test
+    public void getHelloWithName() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello?name=IAST").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello, IAST!")));
     }
 }
